7b57d791cf4033260adb7ff753581f4fa3786945
gomahthy <gomahthy.ganesan@novuna.co.uk>
2023-11-08 06:37:23 +0000

    Jira-101

---
+++ src/main/java/uk/company/jenkinsproj.java
@@ -0,0 +1,11 @@
+package uk.company;
+
+/**
+ * @author Gomahthy Ganesan
+ * Goms_Automation
+ **/
+public class jenkinsproj {
+    public void method() {
+        System.out.println("My method");
+    }
+}
---
+++ src/test/java/JenkinsProjTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author Gomahthy Ganesan
+ * Goms_Automation
+ **/
+public class JenkinsProjTest {
+
+ public void Test(){
+
+ }
+}
